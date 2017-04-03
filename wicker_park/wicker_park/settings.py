5d0dc4c7e0c8907d7e37b7baf2dd1ebec0c5e19c
Griffin Caprio <me@griffincaprio.com>
2017-04-02 19:32:50 -0500

    Easing password requirements. Updating dependencies. Adding django-itunespodcast, serving static files in development

--- wicker_park/wicker_park/settings.py
+++ wicker_park/wicker_park/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'wicker_park',
     'pocket_auth',
+    'podcast',
 ]
 
 MIDDLEWARE = [
@@ -96,12 +97,12 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    #{
+    #    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    #},
+    #{
+    #    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    #},
 ]
 
 
--- wicker_park/wicker_park/urls.py
+++ wicker_park/wicker_park/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 from . import views
 
 urlpatterns = [
     url(r'^pocket_auth/', include('pocket_auth.urls', namespace='pocket_auth')),
     url(r'^admin/', admin.site.urls),
-    url(r'^', views.index, name='index'),
-]
+    url(r'^podcast/', include('podcast.urls')),
+    url(r'^index', views.index, name='index'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
